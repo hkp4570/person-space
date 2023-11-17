81c83a97766bf7dc41fb20a98ea0d536fa182af4
hkp <colourful4570@163.com>
2023-11-17 19:01:01 +0800

    feat: Empty组件

---
+++ src/components/Empty/index.vue
@@ -0,0 +1,35 @@
+<script>
+import Icon from "@/components/Icon/index.vue";
+
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '无数据'
+    }
+  },
+  components: {
+    Icon
+  }
+}
+</script>
+
+<template>
+  <div class="empty-container">
+    <Icon type="empty"/>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import "~@/styles/mixin.less";
+@import "~@/styles/var.less";
+.empty-container {
+  .self-center();
+  color: @gray;
+  text-align: center;
+}
+.iconfont{
+  font-size: 72px;
+}
+</style>
