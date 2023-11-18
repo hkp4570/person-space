7d4ba0cafc86d9658e3bc5503606d2bace1aefb2
hkp <colourful4570@163.com>
2023-11-18 18:26:54 +0800

    feat: 🐙SiteAside左侧导航组件

---
+++ src/components/SiteAside/index.vue
@@ -0,0 +1,51 @@
+<script>
+import Contact from "@/components/SiteAside/Contact/index.vue";
+import Menu from "@/components/SiteAside/Menu/index.vue";
+import Avatar from "@/components/Avatar/index.vue";
+
+export default {
+  components: {
+    Contact,
+    Menu,
+    Avatar,
+  }
+}
+</script>
+
+<template>
+  <div class="site-aside-container">
+    <Avatar url="https://tse2-mm.cn.bing.net/th/id/OIP-C.JuSM25g1b2eEYu-2KbGpVwHaHI?rs=1&pid=ImgDetMain" />
+    <h1 class="title">colourful 的个人空间</h1>
+    <Menu/>
+    <Contact/>
+    <p class="footer">
+      备案
+    </p>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import "~@/styles/var.less";
+
+.site-aside-container {
+  width: 100%;
+  height: 100%;
+  background: @dark;
+  padding: 20px 0;
+  box-sizing: border-box;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+.iconfont {
+  margin: 0 auto;
+}
+.footer {
+  text-align: center;
+  font-size: 12px;
+}
+.title {
+  font-size: 1.2em;
+  color: #fff;
+  text-align: center;
+}
+</style>
---
+++ src/components/SiteAside/test.vue
@@ -0,0 +1,21 @@
+<script>
+import SiteAside from "@/components/SiteAside/index.vue";
+import "@/styles/global.less";
+export default {
+  components: {
+    SiteAside
+  }
+}
+</script>
+
+<template>
+<div class="test-container">
+  <SiteAside />
+</div>
+</template>
+
+<style scoped lang="less">
+.test-container{
+  width: 369px;
+}
+</style>
