949d55b18930a13b5c0936d47ee926d2408e0b5a
hkp <colourful4570@163.com>
2023-12-17 11:55:44 +0800

    feat: 文章详情与目录组件

--- src/pages/Blog/Detail.vue
+++ src/pages/Blog/Detail.vue
@@ -1,15 +1,56 @@
 <script>
+import Layout from '@/components/Layout';
+import BlogDetail from "@/pages/Blog/components/BlogDetail.vue";
+import BlogTOC from "@/pages/Blog/components/BlogTOC.vue";
+import fetchData from "@/mixins/fetchData";
+import {getBlog} from "@/api/blog";
+
 export default {
-  name: "Detail"
+  name: "Detail",
+  mixins: [fetchData(null)],
+  components: {
+    Layout,
+    BlogDetail,
+    BlogTOC
+  },
+  methods:{
+    async fetchData(){
+      return await getBlog(this.$route.params.id);
+    }
+  }
 }
 </script>
 
 <template>
-<div>
-  博客详情
-</div>
+<Layout>
+  <div class="main-container" v-loading="isLoading">
+    <BlogDetail :blog="data" v-if="data" />
+  </div>
+  <template #right>
+    <div class="right-container" v-loading="isLoading">
+      <BlogTOC :toc="data.toc" v-if="data" />
+    </div>
+  </template>
+</Layout>
 </template>
 
 <style scoped lang="less">
-
+.main-container {
+  overflow-y: scroll;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 20px;
+  position: relative;
+  width: 100%;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+}
+.right-container {
+  width: 300px;
+  height: 100%;
+  overflow-y: scroll;
+  box-sizing: border-box;
+  position: relative;
+  padding: 20px;
+}
 </style>
---
+++ src/pages/Blog/components/BlogTOC.vue
@@ -0,0 +1,37 @@
+<script>
+import RightMenu from "@/pages/Blog/components/RightMenu.vue";
+export default {
+  name: "BlogTOC",
+  props: {
+    toc: {
+      type: Array,
+    }
+  },
+  components: {
+    RightMenu
+  },
+  methods: {
+    handleSelect(){
+      location.hash = item.anchor;
+    }
+  }
+}
+</script>
+
+<template>
+<div class="blog-toc-container">
+  <h2>目录</h2>
+  <RightMenu :list="toc" @select="handleSelect" />
+</div>
+</template>
+
+<style scoped lang="less">
+.blog-toc-container {
+  h2 {
+    font-weight: bold;
+    letter-spacing: 2px;
+    font-size: 1em;
+    margin: 0;
+  }
+}
+</style>
