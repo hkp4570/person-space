575e0ef278c8c6b034e92e30b974c9a099520597
colourful <sxhkp163@163.com>
2024-01-07 18:29:14 +0800

    feat: 🐰异步组件，页面添加加载进度

--- src/router.js
+++ src/router.js
@@ -1,6 +1,26 @@
 import Vue from 'vue';
+import "nprogress/nprogress.css";
 import VueRouter from "vue-router";
 import {setRouterTitle} from "@/utils/titleController";
+import { start, done, configure } from "nprogress";
+import {delay} from '@/utils/delay';
+
+configure({
+    trickleSpeed: 20,
+    showSpinner: false,
+});
+
+function getPageComponent(pageCompResolver) {
+    return async () => {
+        start();
+        if (process.env.NODE_ENV === "development") {
+            await delay(2000);
+        }
+        const comp = await pageCompResolver();
+        done();
+        return comp;
+    };
+}
 
 if (!window.VueRouter) {
     Vue.use(VueRouter);
@@ -11,49 +31,49 @@ const router = new VueRouter({
         {
             name: 'Home',
             path: '/',
-            component: () => import(/* webpackChunkName: "home" */ "@/pages/Home"),
+            component: getPageComponent(() => import(/* webpackChunkName: "home" */ "@/pages/Home")),
             meta: {title: '首页'}
         },
         {
             name: 'About',
             path: '/about',
-            component: () => import(/* webpackChunkName: "about" */ "@/pages/About"),
+            component: getPageComponent(() => import(/* webpackChunkName: "about" */ "@/pages/About")),
             meta: {title: '关于我'}
         },
         {
             name: 'Blog',
             path: '/blog',
-            component: () => import(/* webpackChunkName: "blog" */ "@/pages/Blog"),
+            component: getPageComponent(() => import(/* webpackChunkName: "blog" */ "@/pages/Blog")),
             meta: {title: '文章'}
         },
         {
             name: 'CategoryBlog',
             path: '/blog/cate/:categoryId',
-            component: () => import(/* webpackChunkName: "categoryBlog" */ "@/pages/Blog"),
+            component: getPageComponent(() => import(/* webpackChunkName: "categoryBlog" */ "@/pages/Blog")),
             meta: {title: '文章分类'}
         }, // 动态路由
         {
             name: 'BlogDetail',
             path: '/blog/:id',
-            component: () => import(/* webpackChunkName: "blogDetail" */ "@/pages/Blog/Detail.vue"),
+            component: getPageComponent(() => import(/* webpackChunkName: "blogDetail" */ "@/pages/Blog/Detail.vue")),
             meta: {title: '文章详情'}
         },
         {
             name: 'Message',
             path: '/message',
-            component: () => import(/* webpackChunkName: "message" */ "@/pages/Message"),
+            component: getPageComponent(() => import(/* webpackChunkName: "message" */ "@/pages/Message")),
             meta: {title: '留言板'}
         },
         {
             name: 'Project',
             path: '/project',
-            component: () => import(/* webpackChunkName: "project" */ "@/pages/Project"),
+            component: getPageComponent(() => import(/* webpackChunkName: "project" */ "@/pages/Project")),
             meta: {title: '项目&效果'}
         },
         {
             name: 'Demo',
             path: '/demo',
-            component: () => import(/* webpackChunkName: "demo" */ "@/pages/Demo"),
+            component: getPageComponent(() => import(/* webpackChunkName: "demo" */ "@/pages/Demo")),
             meta: {title: '组件通信demo'}
         },
     ],
---
+++ src/utils/delay.js
@@ -0,0 +1,4 @@
+
+export const delay = (duration = 1000) => {
+    return new Promise(resolve => setTimeout(resolve,duration));
+}
